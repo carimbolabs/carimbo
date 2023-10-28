0c17f18da59043b043ba571f413576e7b7efa4f6
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-10-28 12:03:34 -0300

    Work in progress

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -2,6 +2,6 @@
 
 void filesystem::mount(const std::string &filename, const std::string &mountpoint) {
   if (PHYSFS_mount(filename.c_str(), mountpoint.c_str(), true) == 0) {
-    throw std::runtime_error(fmt::format("Failed to mount {} to {}", filename, mountpoint));
+    throw std::runtime_error(fmt::format("[PHYSFS_mount] Failed to mount {} to {}", filename, mountpoint));
   }
 }
--- src/pixmap.cpp
+++ src/pixmap.cpp
@@ -2,30 +2,26 @@
 
 pixmap::pixmap(const std::shared_ptr<renderer> renderer, const std::string &filename) : _renderer(renderer) {
   const auto buffer = file::read(filename);
-
   const auto decoder = avifDecoderCreate();
 
-  //   rgb.depth = 8;
-  // #if SDL_BYTEORDER == SDL_LIL_ENDIAN
-  //   rgb.format = AVIF_RGB_FORMAT_BGRA;
-  // #else
-  //   rgb.format = AVIF_RGB_FORMAT_ARGB;
-  // #endif
-
   if (avifDecoderSetIOMemory(decoder, reinterpret_cast<const uint8_t *>(&buffer[0]), buffer.size()) != AVIF_RESULT_OK) {
+    avifDecoderDestroy(decoder);
     throw std::runtime_error(fmt::format("[avifDecoderSetIOMemory] Error while setting IO on avifDecoder: {}", filename));
   }
 
   if (avifDecoderParse(decoder) != AVIF_RESULT_OK) {
+    avifDecoderDestroy(decoder);
     throw std::runtime_error(fmt::format("[avifDecoderParse] Error while parsing avifDecoder: {}", filename));
   }
 
   if (avifDecoderNextImage(decoder) != AVIF_RESULT_OK) {
+    avifDecoderDestroy(decoder);
     throw std::runtime_error(fmt::format("[avifDecoderNextImage] Error while decoding avifDecoder: {}", filename));
   }
 
   _width = decoder->image->width;
   _height = decoder->image->height;
+
   const auto surface = SDL_CreateRGBSurfaceWithFormat(0, _width, _height, 0, SDL_PIXELFORMAT_ARGB8888);
 
   avifRGBImage rgb{};
@@ -41,7 +37,6 @@ pixmap::pixmap(const std::shared_ptr<renderer> renderer, const std::string &file
   rgb.rowBytes = (uint32_t)surface->pitch;
 
   if (avifImageYUVToRGB(decoder->image, &rgb) != AVIF_RESULT_OK) {
-    avifRGBImageFreePixels(&rgb);
     avifDecoderDestroy(decoder);
     SDL_FreeSurface(surface);
     throw std::runtime_error(fmt::format("[avifImageYUVToRGB] Error while converting YUV to RGB on avifImage: {}", filename));
@@ -50,14 +45,13 @@ pixmap::pixmap(const std::shared_ptr<renderer> renderer, const std::string &file
   _texture = texture_ptr(SDL_CreateTextureFromSurface(*renderer, surface), SDL_Deleter());
 
   if (_texture == nullptr) {
-    avifRGBImageFreePixels(&rgb);
     avifDecoderDestroy(decoder);
+    SDL_FreeSurface(surface);
     throw std::runtime_error(fmt::format("[SDL_CreateTextureFromSurface] Error while creating texture from surface: {}", filename));
   }
 
-  SDL_FreeSurface(surface);
-  // avifRGBImageFreePixels(&rgb);
   avifDecoderDestroy(decoder);
+  SDL_FreeSurface(surface);
 }
 
 void pixmap::draw(const int x, const int y) const {
