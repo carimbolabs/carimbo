7082c2b9c9eeedb47c289a2926f3cba7f71eb984
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-10-28 11:54:46 -0300

    Work in progress

---
+++ src/deleters.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef _deleters_hpp
+#define _deleters_hpp
+
+#include "internal.hpp"
+
+struct SDL_Deleter {
+  inline void operator()(SDL_Surface *ptr) {
+    SDL_FreeSurface(ptr);
+  }
+
+  inline void operator()(SDL_Texture *ptr) {
+    SDL_DestroyTexture(ptr);
+  }
+
+  inline void operator()(SDL_Renderer *ptr) {
+    SDL_DestroyRenderer(ptr);
+  }
+
+  inline void operator()(SDL_Window *ptr) {
+    SDL_DestroyWindow(ptr);
+  }
+
+  inline void operator()(SDL_RWops *ptr) {
+    SDL_RWclose(ptr);
+  }
+};
+
+#endif
