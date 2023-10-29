d146821e3f781f908acab067ca83d1d8b28824b2
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-10-29 13:07:36 -0300

    Work in progress

--- src/common.hpp
+++ src/common.hpp
@@ -2,12 +2,12 @@
 #ifndef _common_hpp
 #define _common_hpp
 
-#include "SDL2/SDL.h"
 #include "avif/avif.h"
 #include "curl/curl.h"
 #include "fmt/core.h"
 #include "physfs.h"
 #include "sol/sol.hpp"
+#include "SDL2/SDL.h"
 
 #include <cassert>
 #include <iostream>
