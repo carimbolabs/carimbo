8378e01fc1367f0dc7aacf035e0874c547b5bb18
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-11-07 16:21:03 -0300

    Work in progress

--- assets/demo/scripts/main.lua
+++ assets/demo/scripts/main.lua
@@ -4,47 +4,47 @@ local engine = EngineFactory.new()
     :set_height(600)
     :create()
 
-engine:prefetch({ "blob/matrix.avif" })
+-- engine:prefetch({ "blob/matrix.avif" })
 
-local entity = engine:spawn()
+-- local entity = engine:spawn()
 
-entity:set_pixmap("blob/matrix.avif")
+-- entity:set_pixmap("blob/matrix.avif")
 
-local angle = 0
+-- local angle = 0
 
-entity:on_update(function(self)
-  if engine:is_keydown(KeyEvent.w) then
-    self.y = self.y - 1
-  end
+-- entity:on_update(function(self)
+--   if engine:is_keydown(KeyEvent.w) then
+--     self.y = self.y - 1
+--   end
 
-  if engine:is_keydown(KeyEvent.a) then
-    self.x = self.x - 1
-  end
+--   if engine:is_keydown(KeyEvent.a) then
+--     self.x = self.x - 1
+--   end
 
-  if engine:is_keydown(KeyEvent.s) then
-    self.y = self.y + 1
-  end
+--   if engine:is_keydown(KeyEvent.s) then
+--     self.y = self.y + 1
+--   end
 
-  if engine:is_keydown(KeyEvent.d) then
-    self.x = self.x + 1
-  end
+--   if engine:is_keydown(KeyEvent.d) then
+--     self.x = self.x + 1
+--   end
 
-  angle = angle + 1
-  if angle > 360 then
-    angle = 0
-  end
+--   angle = angle + 1
+--   if angle > 360 then
+--     angle = 0
+--   end
 
-  self.angle = angle
-end)
+--   self.angle = angle
+-- end)
 
-local gc = engine:spawn()
+-- local gc = engine:spawn()
 
-gc:on_update(function(self)
-  if collectgarbage("count") / 1024 > 16 then
-    collectgarbage("collect")
-  else
-    collectgarbage("step", 1)
-  end
-end)
+-- gc:on_update(function(self)
+--   if collectgarbage("count") / 1024 > 16 then
+--     collectgarbage("collect")
+--   else
+--     collectgarbage("step", 1)
+--   end
+-- end)
 
 engine:run()
