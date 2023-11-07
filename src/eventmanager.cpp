3bb3601e873aefc1292228862a012456b746a0b8
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-11-07 12:22:32 -0300

    Work in progress

--- src/eventmanager.cpp
+++ src/eventmanager.cpp
@@ -22,6 +22,11 @@ void eventmanager::update() {
       std::for_each(_receivers.begin(), _receivers.end(),
                     std::bind(&eventreceiver::on_keyup, std::placeholders::_1, keyevent(event.key.keysym.sym)));
       break;
+
+    case SDL_JOYAXISMOTION:
+      const auto joy = (SDL_JoyAxisEvent *)&event;
+      std::cout << "which: " << joy->which << "axis: " << joy->axis << "value: " << joy->value << std::endl;
+      break;
     }
   }
 }
