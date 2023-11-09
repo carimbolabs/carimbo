e250e6439e1a9497048af5ef41c8ae7bf4cd2a04
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-11-09 07:41:32 -0300

    Work in progress

--- assets/demo/scripts/main.lua
+++ assets/demo/scripts/main.lua
@@ -5,55 +5,55 @@ local engine = EngineFactory.new()
     :set_fullscreen(false)
     :create()
 
--- engine:prefetch({ "blob/matrix.avif" })
+engine:prefetch({ "blob/matrix.avif" })
 
--- local entity = engine:spawn()
+local entity = engine:spawn()
 
--- entity:set_pixmap("blob/matrix.avif")
+entity:set_pixmap("blob/matrix.avif")
 
--- local angle = 0
--- local alpha = 0
+local angle = 0
+local alpha = 0
 
--- entity:on_update(function(self)
---   if engine:is_keydown(KeyEvent.w) then
---     self.y = self.y - 1
---   end
+entity:on_update(function(self)
+  if engine:is_keydown(KeyEvent.w) then
+    self.y = self.y - 1
+  end
 
---   if engine:is_keydown(KeyEvent.a) then
---     self.x = self.x - 1
---   end
+  if engine:is_keydown(KeyEvent.a) then
+    self.x = self.x - 1
+  end
 
---   if engine:is_keydown(KeyEvent.s) then
---     self.y = self.y + 1
---   end
+  if engine:is_keydown(KeyEvent.s) then
+    self.y = self.y + 1
+  end
 
---   if engine:is_keydown(KeyEvent.d) then
---     self.x = self.x + 1
---   end
+  if engine:is_keydown(KeyEvent.d) then
+    self.x = self.x + 1
+  end
 
---   angle = angle + 1
---   if angle > 360 then
---     angle = 0
---   end
+  angle = angle + 1
+  if angle > 360 then
+    angle = 0
+  end
 
---   self.angle = angle
+  self.angle = angle
 
---   alpha = alpha + 1
---   if alpha > 255 then
---     alpha = 0
---   end
+  alpha = alpha + 1
+  if alpha > 255 then
+    alpha = 0
+  end
 
---   self.alpha = alpha
--- end)
+  self.alpha = alpha
+end)
 
--- local gc = engine:spawn()
+local gc = engine:spawn()
 
--- gc:on_update(function(self)
---   if collectgarbage("count") / 1024 > 16 then
---     collectgarbage("collect")
---   else
---     collectgarbage("step", 1)
---   end
--- end)
+gc:on_update(function(self)
+  if collectgarbage("count") / 1024 > 16 then
+    collectgarbage("collect")
+  else
+    collectgarbage("step", 1)
+  end
+end)
 
 engine:run()
