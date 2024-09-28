3d2c54b43ab0b9ee0e9d975667cf4d91d716650b
Rodrigo Delduca <rodrigo@delduca.org>
2024-09-28 10:07:37 -0300

    Work in progress

--- src/entitymanager.cpp
+++ src/entitymanager.cpp
@@ -15,7 +15,34 @@ void entitymanager::set_resourcemanager(
 
 std::shared_ptr<entity> entitymanager::spawn(const std::string_view id) {
   const auto buffer = storage::io::read(fmt::format("entities/{}.json", id));
+  json j = buffer;
 
+  std::cout << ">>> " << j["spritesheet"] << std::endl;
+  /*
+  namespace framework {
+  struct keyframe {
+    geometry::rect frame;
+    uint64_t duration;
+
+    keyframe() = default;
+  };
+
+  struct entityprops {
+  public:
+    std::shared_ptr<graphics::pixmap> spritesheet;
+    std::map<std::string, std::vector<keyframe>> frames;
+    geometry::point position;
+    geometry::point pivot;
+    float_t angle;
+    graphics::flip flip;
+    uint8_t alpha;
+    std::string_view id;
+
+    entityprops() = default;
+  };
+  }
+
+  */
   const auto e = entity::create("abc");
   std::cout << "[entitymanager] spawn: " << e->id() << std::endl;
   _entities.emplace_back(e);
--- src/entityprops.hpp
+++ src/entityprops.hpp
@@ -12,9 +12,6 @@ struct keyframe {
   uint64_t duration;
 
   keyframe() = default;
-
-  keyframe(const geometry::rect &frame, uint64_t duration)
-      : frame(frame), duration(duration) {}
 };
 
 struct entityprops {
@@ -29,22 +26,5 @@ public:
   std::string_view id;
 
   entityprops() = default;
-
-  entityprops(std::shared_ptr<graphics::pixmap> spritesheet,
-              std::map<std::string, std::vector<keyframe>> frames,
-              geometry::point position = {},
-              geometry::point pivot = {},
-              float_t angle = 0.0f,
-              graphics::flip flip = graphics::flip::none,
-              uint8_t alpha = 255,
-              std::string id = "")
-      : spritesheet(std::move(spritesheet)),
-        frames(std::move(frames)),
-        position(std::move(position)),
-        pivot(std::move(pivot)),
-        angle(angle),
-        flip(flip),
-        alpha(alpha),
-        id(std::move(id)) {}
 };
 }
