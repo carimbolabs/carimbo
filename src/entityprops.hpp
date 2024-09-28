f67fcd8d4a9f1660c976e5c6cf6387e7cb61f3a4
Rodrigo Delduca <rodrigo@delduca.org>
2024-09-28 07:00:37 -0300

    Work in progress

--- src/entityprops.hpp
+++ src/entityprops.hpp
@@ -2,16 +2,13 @@
 
 #include "common.hpp"
 
-#include "pixmap.hpp"
-#include "point.hpp"
-
 namespace framework {
 struct entityprops {
-  std::string id;
-  std::shared_ptr<graphics::pixmap> pixmap;
+  std::unique_ptr<graphics::pixmap> pixmap;
   geometry::point point;
-  double_t angle;
+  float_t angle;
   graphics::flip flip;
   uint8_t alpha;
+  std::string_view id;
 };
 }
