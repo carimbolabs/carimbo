c75d6e1be71f06ddf69ad7b362bd33d3cf936553
Rodrigo Delduca <rodrigo@delduca.org>
2024-11-27 09:41:16 -0300

    Work in progress

--- src/world.cpp
+++ src/world.cpp
@@ -36,72 +36,54 @@ space_ptr world::space() const noexcept {
 void world::update(float_t delta) const noexcept {
   UNUSED(delta);
 
-  cpSpaceStep(_space.get(), 1.0 / 60.0);
+  cpSpaceStep(_space.get(), delta > 0 ? delta : 1.0 / 60.0);
+  // cpSpaceStep(_space.get(), 1.0 / 60.0);
 }
 
 void world::draw() noexcept {
   auto drawShape = [&](cpShape *shape) {
-    const cpBody *body = cpShapeGetBody(shape);
-    const cpVect position = cpBodyGetPosition(body);
+    const auto *body = cpShapeGetBody(shape);
+    const auto position = cpBodyGetPosition(body);
 
-    int count = cpPolyShapeGetCount(shape);
-    std::vector<cpVect> vertices(count);
-    std::generate(vertices.begin(), vertices.end(), [&, i = 0]() mutable {
+    const int n = cpPolyShapeGetCount(shape);
+    std::vector<cpVect> vertices(n);
+    std::ranges::generate(vertices, [&, i = 0]() mutable {
       return cpPolyShapeGetVert(shape, i++);
     });
 
-    uint8_t r, g, b, a = 255;
-    switch (cpShapeGetCollisionType(shape)) {
-    case 1:
-      r = 255;
-      g = 0;
-      b = 0;
-      break;
-    case 2:
-      r = 0;
-      g = 0;
-      b = 255;
-      break;
-    case 3:
-      r = 255;
-      g = 255;
-      b = 0;
-      break;
-    case 4:
-      r = 0;
-      g = 255;
-      b = 255;
-      break;
-    case 5:
-      r = 0;
-      g = 255;
-      b = 0;
-      break;
-    default:
-      r = 255;
-      g = 255;
-      b = 255;
-      break;
-    }
+    auto [r, g, b] = [](cpCollisionType type) -> std::tuple<uint8_t, uint8_t, uint8_t> {
+      switch (type) {
+      case 1:
+        return {255, 0, 0};
+      case 2:
+        return {0, 0, 255};
+      case 3:
+        return {255, 255, 0};
+      case 4:
+        return {0, 255, 255};
+      case 5:
+        return {0, 255, 0};
+      default:
+        return {255, 255, 255};
+      }
+    }(cpShapeGetCollisionType(shape));
 
-    SDL_SetRenderDrawColor(*_renderer.get(), r, g, b, a);
+    SDL_SetRenderDrawColor(*_renderer, r, g, b, 255);
 
-    for (int i = 0; i < count; ++i) {
-      int next = (i + 1) % count;
-
+    for (int i = 0; i < n; ++i) {
+      int next = (i + 1) % n;
       int x1 = static_cast<int>(position.x + vertices[i].x);
       int y1 = static_cast<int>(position.y + vertices[i].y);
       int x2 = static_cast<int>(position.x + vertices[next].x);
       int y2 = static_cast<int>(position.y + vertices[next].y);
-
-      SDL_RenderDrawLine(*_renderer.get(), x1, y1, x2, y2);
+      SDL_RenderDrawLine(*_renderer, x1, y1, x2, y2);
     }
   };
 
   cpSpaceEachShape(
-      _space.get(), [](cpShape *shape, void *data) {
-        auto draw = static_cast<decltype(drawShape) *>(data);
-        (*draw)(shape);
+      _space.get(),
+      [](cpShape *shape, void *data) {
+        (*static_cast<decltype(drawShape) *>(data))(shape);
       },
       &drawShape
   );
